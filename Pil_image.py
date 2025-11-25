2a74a352b2d60ca2d8da54d7e785e03f257f1472
Akash Yadav <akashyadav6592@gmail.com>
2025-11-25 18:19:06 +0530

    Update Pil_image.py

--- Pil_image.py
+++ Pil_image.py
@@ -1,13 +1,11 @@
 from PIL import Image
 import os
 
-# Step 1: इमेज का पाथ दें
-image_path = 'path/to/your/image.jpg'  # उदाहरण: 'C:/Users/YourName/Pictures/photo.jpg'
+image_path = 'path/to/your/image.jpg'
 
-# Step 2: इमेज खोलें
 img = Image.open(image_path)
 
-# Step 3: Resize करें (width, height)
+
 new_size = (300, 300)
 resized_img = img.resize(new_size)
 
@@ -16,3 +14,4 @@ save_path = 'path/to/save/resized_image.jpg'
 resized_img.save(save_path)
 
 print("Image resized and saved successfully!")
+
